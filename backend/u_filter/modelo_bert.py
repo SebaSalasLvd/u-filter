c4c3bc80bda97f77e6f99b9bec901830dd935c4a
Aerodinamica <nicolas.inostroza.n@ug.uchile.cl>
2025-09-21 19:46:11 -0300

    Agregue comentarios y arreglue el requirements.txt para que tenga rest_framework para hacer la api

--- backend/u_filter/modelo_bert.py
+++ backend/u_filter/modelo_bert.py
@@ -1,12 +1,13 @@
 from transformers import pipeline
 
-# Load Spanish BERT pretrained model for text classification
+# BERT entrenado en español de chile este es el que deberiamos usar pero necesita fine-tuning
 bert_pipeline = pipeline(
     "text-classification",
     model="dccuchile/bert-base-spanish-wwm-cased",
     tokenizer="dccuchile/bert-base-spanish-wwm-cased"
 )
 
+#BERT pre-entrenado para multiples lenguajes este es de base para que responda algo por mientras
 bert_pipeline_test = pipeline(
     "sentiment-analysis",
     model="nlptown/bert-base-multilingual-uncased-sentiment"
--- backend/u_filter/views.py
+++ backend/u_filter/views.py
@@ -4,13 +4,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from .modelo_bert import bert_pipeline_test
 
+#Ignorar era para testear
 class HelloView(APIView):
     def get(self, request):
         return Response({"message": "Hello from Django REST Framework!"})
 
+#La vista de BERT esto sera nuestra punto de llamada al modelo para devolver la respuesta al front
 class BERTView(APIView):
     def post(self, request):
-        text = request.data.get("text", None)
+        text = request.data.get("text", None) #Esto se cambia despues segun como hagamos el front
         if not text:
             return Response({"error": "No text provided"}, status=status.HTTP_400_BAD_REQUEST)
 
