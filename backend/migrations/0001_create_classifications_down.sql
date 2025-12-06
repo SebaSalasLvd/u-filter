0eeecdaa8dd610b887b1692252d5b611105003f4
Sebastián Salas Lavado <sebastian.salas.l@ug.uchile.cl>
2025-12-06 06:14:04 -0300

    refactor(core): added links to migration, refactored posts

--- backend/migrations/0001_create_classifications_down.sql
+++ backend/migrations/0001_create_classifications_down.sql
@@ -1,4 +1,6 @@
 -- Migration: 0001_create_classifications_down.sql
 -- Reversa de la migración 0001_create_classifications_up.sql
 
-DROP TABLE IF EXISTS classifications;
+DROP TABLE IF EXISTS posts;
+
+DROP TABLE IF EXISTS links;
